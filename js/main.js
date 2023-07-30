06dcbb3c65175fdb751a0d9388a0954ab3c20281
Roberto Salama <102178835+ROBER-SALAMA@users.noreply.github.com>
2023-07-30 11:08:28 -0600

    agregue la funcion para hacer la busqueda usando la api de github

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 const API = "https://api.github.com/users/";
 
+// funcio para hacer la busqueda
+async function doSearch (){
+    const response = await fetch (API + 'ROBER-SALAMA') //hacemos uso de la API + nombre de usuario
+    const data = await response.json()
+    console.log(response);
+}
 
 // crea una instancia vue
 const App = Vue.createApp({
