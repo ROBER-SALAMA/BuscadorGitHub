b719b2ad4abce0c56a1876dbaca8aa341885c7cc
Roberto Salama <102178835+ROBER-SALAMA@users.noreply.github.com>
2023-08-01 21:32:40 -0600

    hice modificaciones al metodo doSearch

--- js/main.js
+++ js/main.js
@@ -3,22 +3,37 @@ const API = "https://api.github.com/users/";
 
 // crea una instancia vue
 const App = Vue.createApp({
-    data(){
-        return{
+    data() {
+        return {
             // modelo search
             search: null,
             // modelo result
-            result: null
+            result: null,
+            // modelo error
+            error: null
         };
     },
     //metodo para hacer la busqueda
-    methods:{
-        async doSearch (){
-            const response = await fetch (API + this.search) //hacemos uso de la API + el modelo search
-            const data = await response.json()
-            console.log(data);
-            // si se busca algo dejara de ser nulo y lo mostrara
-            this.result = true
+    methods: {
+        async doSearch() {
+            // limpia las propiedades
+            this.result = this.error = null
+            try {
+                const response = await fetch(API + this.search) //hacemos uso de la API + el modelo search
+                // si la respuesta no es ok mostrara el error
+                if(!response.ok) throw new Error("User not fout")
+                const data = await response.json()
+                console.log(data);
+                // si se busca algo dejara de ser nulo y lo mostrara
+                this.result = true
+            } catch (error) {
+                // el this.error es la propieddd del modelo, y el "error" es lo que le vamos
+                // a asignar dentro si sucede algo dentro del bloque try
+                this.error = error
+            }finally{
+                // si se busca algo y todo sale bien se limpia la caja de busqueda
+                this.search = null
+            }
         }
     },
 })
